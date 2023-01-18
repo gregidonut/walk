da808b71d0ca9e817f38e1caa89907b88c970675
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-18 23:29:09 +0800

    docs: add a little bit of a comment documentation on filterOut()
    it's a tiny algorithm but i think it helps a little bit for someone
    trying to debug this if any problems shall arise.

--- actions.go
+++ actions.go
@@ -8,6 +8,14 @@ import (
 )
 
 func filterOut(path, ext string, minSize int64, info os.FileInfo) bool {
+	// sad paths return true to simulate a continue statement in a forloop
+	// that is checked by the calling function Run()
+	// in other words if these conditions are met
+	// i.e. path is a directory or size is less than minimum size specified
+	// or extension specified does not match the file extension we will
+	// *continue* otherwise we will return false and the calling function
+	// in Run() will proceed with the next step which is to presumably list
+	// the file if this function returns false
 	if info.IsDir() || info.Size() < minSize {
 		return true
 	}
