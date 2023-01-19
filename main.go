6161d1645f0a4ffd437f13135b2404475ef4bfc6
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-19 17:45:44 +0800

    feat: add logic to call the delFile() in main.go
    first added that extra flag to parse (-del) then also added a
    corresponding field in the config struct anc instance in main as it will
    be used in the run function

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ type config struct {
 	size int64
 	// list files
 	list bool
+	// delete files
+	del bool
 }
 
 func main() {
@@ -22,6 +24,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
@@ -31,6 +34,7 @@ func main() {
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -52,6 +56,10 @@ func run(root string, out io.Writer, cfg config) error {
 		// If list was explicitly set, don't do anything else
 		if cfg.list {
 			return listFile(path, out)
+		}
+
+		if cfg.del {
+			return delFile(path)
 		}
 
 		// List is the default option if nothing else was set
